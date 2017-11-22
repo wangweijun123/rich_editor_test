fa62acdbd4693271e5ea4a4165cd4da14e5add4c
wangweijun <262448350@qq.com>
2017-11-23 00:15:02 +0800

    test

--- app/src/main/java/com/richeditor/wangweijun1/richeditortest/MainActivity.java
+++ app/src/main/java/com/richeditor/wangweijun1/richeditortest/MainActivity.java
@@ -222,9 +222,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                mEditor.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
-//                        "dachshund");
-                choseHeadImageFromPicture();
+                mEditor.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
+                        "dachshund");
+//                choseHeadImageFromPicture();
             }
         });
 
